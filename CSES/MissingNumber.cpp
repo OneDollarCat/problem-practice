064997a047f7dab288bbd633ec4ea902a4ea6954
= <=>
2025-09-30 17:56:07 +0530

    Add files

---
+++ CSES/MissingNumber.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+   long long n;
+   cin >> n;
+   long long el;
+   vector<int> nums;
+   for (long long i=0; i<n-1; i++) {
+      cin >> el;
+      nums.push_back(el);
+   }
+   long long missing;
+   int expectSum = (n * (n + 1)) / 2;
+   int realSum = 0;
+   for (int i : nums) {
+      realSum += i;
+   }
+   missing = expectSum - realSum;
+   cout << missing;
+   return 0;
+}
+
